5a8db9c80a9709cdffc2cc14d9600685ac33aee1
DanielStyles3 <danielstyles318@gmail.com>
2023-07-20 11:38:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'Daniel Styles',
+        bio: "Hi there! 👋 I'm Daniel, a passionate web developer with a love for creating innovative and user-friendly digital experiences.",
+        imgSrc: "./images/styles.jpg ",
+        profession: 'Web Developer'
+      },
+      show: false,
+      timeInterval: 0
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({
+        timeInterval: prevState.timeInterval + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show
+    }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, timeInterval } = this.state;
+
+    return (
+      <div className='App'>
+        <button className='button' onClick={this.toggleShow}><span>Toggle Profile</span></button>
+        {show && (
+          <div>
+            <h2>{fullName}</h2>
+            <img src={imgSrc} alt="Profile" />
+            <p>Bio: {bio}</p>
+            <p>Profession: {profession}</p>
+          </div>
+        )}
+        <p>Time Interval: {timeInterval} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
